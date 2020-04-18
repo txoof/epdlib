7c6045efd76ba8c8078f7ebd808833f052ebe1ab
txoof <aaron.ciuffo@gmail.com>
2020-04-18 12:41:40 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,29 +1,25 @@
 import setuptools
 
+# import most of the setup vars from here
+from constants import *
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="epdlib", # Replace with your own username
-    version="0.0.1.5",
-    author="Aaron Ciuffo",
-    author_email="aaron.ciuffo@gmail.com",
-    description="EpdLib is a library for creating dynamically scaled screen layouts for frame-buffered devices such as e-paper/e-ink displays.",
+    name=name,
+    version=version,
+    author=author,
+    author_email=author_email,
+    description=description,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/txoof/epdlib",
+    url=url,
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: OS Independent",
-    ],
-    keywords="graphics e-paper display waveshare",
-    install_requires=["Pillow"],
-    project_urls={"Source": "https://github.com/txoof/epdlib"},
-    python_requires='>=3.7',
+    classifiers=classifiers,
+    keywords=keywords,
+    install_requires=install_requires,
+    project_urls=project_urls,
+    python_requires=python_requires,
     package_data={"documentation": ["./docs"]},
 )
