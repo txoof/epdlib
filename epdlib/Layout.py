5b2a75affff25a8d74fa83c7ca2bed9768b04748
txoof <aaron.ciuffo@gmail.com>
2023-09-17 11:50:48 +0200

    resolve I#51 - allow update of resolution

--- epdlib/Layout.py
+++ epdlib/Layout.py
@@ -69,11 +69,14 @@ def strict_enforce(*types):
 
 
 class Layout:
-    def __init__(self, resolution, layout=None, force_onebit=False, mode='1'):
+    def __init__(self, resolution, layout=None, force_onebit=False, mode=None):
+        
+        if mode is None:
+            mode = '1'
+        
         self.resolution = resolution
         self.force_onebit = force_onebit
         self.mode = mode
-        self.blocks = {}
         self.layout = layout
         
     @property
@@ -89,6 +92,13 @@ class Layout:
         
         self._resolution = resolution
         
+        # force an update to the layout when the resolution is reset
+        try:
+            # deepcopy the master_layout into the layout property and preserve the layout
+            self.layout = copy.deepcopy(self._master_layout)
+        except AttributeError:
+            pass
+        
     @property
     def layout(self):
         return self._layout
@@ -96,14 +106,18 @@ class Layout:
     @layout.setter
     @strict_enforce((dict, type(None)))
     def layout(self, layout):
-        '''set the layout property and creates the blocks if layout is provided
+        '''set the layout property, calculates positions and creates the blocks 
         
         Sets:
-            self.blocks'''
-        self._layout = layout
+            self.layout
+            self.blocks
+            '''
+        # deep copy the provided layout into the 
+        self._master_layout = copy.deepcopy(layout)
+        self.blocks = {}
+
         
-        if self._layout:
-            self._add_defaults()
+        if self._master_layout:
             self._calculate_layout()
             
             blocks = {}
@@ -112,6 +126,8 @@ class Layout:
             for name, values in self.layout.items():
                 blocks[name] = self.set_block(name, values)
             self.blocks = blocks
+        else:
+            logging.debug('NO MASTER LAYOUT YET')
 
 
     def set_block(self, name, values, force_recalc=False):
@@ -124,13 +140,6 @@ class Layout:
             name(str): reference name for block
             values(dict): settings for block
             force_recalc(bool): force recalculation of all the blocks'''
-#         if not self.layout:
-#             return
-        
-        
-
-        
-#         for section, vals in self.layout.items():
         logging.info(f'setting section: [{name:_^30}]')
         
         
@@ -165,6 +174,12 @@ class Layout:
 
     def _add_defaults(self):
         '''check that layout contains the minimum default values '''
+        try:
+            if not self._layout:
+                return
+        except AttributeError:
+            return
+
         logging.debug('[[----checking default values for layout----]')
         for section, values in self.layout.items():
             logging.debug(f'section: [{section:-^30}]')
@@ -172,17 +187,7 @@ class Layout:
             
             if not 'type' in values:
                 logging.critical(f'epdlib v{version.__version__}: section "{section}" is missing key "type". As of v0.6 all layout sections must include the key "type". Please see v0.5 changelog')
-                raise KeyError(f'section "{section}" is missing key "type"! Each section in the layout must have the correct block type')
-                
-#                 ## backwards compatibility for pre v0.5 layouts -- remove this in v0.6
-#                 logging.warning(f'guessing block type for section "{section}"')
-#                 if values['image']:
-#                     my_type = 'ImageBlock'
-#                 else: 
-#                     my_type = 'TextBlock'              
-#                 logging.warning(f'guessed: {my_type} -- if this is incorrect add the key "type" with the appropriate Block type in this section of the layout.')
-#                 values['type'] = my_type
-                ## end backwards compatibility
+                raise KeyError(f'section "{section}" is missing key "type"! Each section in the layout must have an explicit block type')
                 
             else:
                 my_type = values['type']
@@ -209,9 +214,14 @@ class Layout:
             
     def _calculate_layout(self):
         '''calculate values for each block based on resolution, absolute and relative positions'''
-        if not self._layout:
+        
+        try:
+            # always start with a fresh copy of the `master_layout` and recalculate everything
+            self._layout = copy.deepcopy(self._master_layout)
+        except AttributeError:
             return
         
+        self._add_defaults()
         
         logging.debug('[[....calculating layouts....]]')
         for section, values in self.layout.items() :
@@ -227,15 +237,12 @@ class Layout:
             
             padded_area = (area[0] - (2* values['padding']),
                            area[1] - (2* values['padding']))
-    
+        
             values['area'] = area
             values['padded_area'] = padded_area
-            
-#             # scale the selected font face size into the available area/lines
-#             if values['type'] == 'TextBlock':
-#                 values['font_size'] = self._scale_font(values)
-            
-            
+            logging.debug(f'area: {area}, padded_area: {padded_area}')
+        
+        
             # calculate absolute position for each block using the relative positions of reference block(s)
             if values['abs_coordinates'][0] is None or values['abs_coordinates'][1] is None:
                 logging.debug('calculating block position from relative positions')
@@ -330,5 +337,146 @@ class Layout:
                 self.image.paste(self.blocks[b].image, self.blocks[b].abs_coordinates)
         return self.image    
             
+
+
+
+
+
+
+# from random import randint, choice
+# from IPython.display import display
+# from time import sleep
+# bogus_layout = {
+#     'l_head': {          
+#         'type': 'TextBlock',
+#         'image': None,
+#         'max_lines': 1,
+#         'width': .5,
+#         'height': .1,
+#         'abs_coordinates': (0, 0),
+#         'rand': True,
+#         'font': '../fonts/Open_Sans/OpenSans-Regular.ttf',
+#         'bkground': 'BLACK',
+#         'fill': 'WHITE'
+#     },
+#     'r_head': {          
+#         'type': 'TextBlock',
+#         'image': None,
+#         'max_lines': 1,
+#         'width': .5,
+#         'height': .1,
+#         'abs_coordinates': (None, 0),
+#         'relative': ('l_head', 'r_head'),
+#         'rand': True,
+#         'font': '../fonts/Open_Sans/OpenSans-Regular.ttf',
+#         'bkground': 'RED',
+#         'fill': 'BLACK'
+#     },
+
+#     'number': {
+#         'type': 'TextBlock',
+#         'image': None,
+#         'max_lines': 1,
+#         'width': .6,
+#         'height': .4,
+#         'abs_coordinates': (0, None),
+#         'relative': ('number', 'l_head'),
+#         'rand': True,
+#         'font': '../fonts/Open_Sans/OpenSans-Regular.ttf',
+#     },
+#     'small_number': {
+#         'type': 'TextBlock',
+#         'image': None,
+#         'max_lines': 1,
+#         'width': .4,
+#         'height': .5,
+#         'abs_coordinates': (None, None),
+#         'relative': ('number', 'l_head'),
+#         'rand': True,
+#         'font': '../fonts/Open_Sans/OpenSans-Regular.ttf',
+#         'fill': 'BLUE',
+#         'bkground': 'GREEN',
+#         'rgb_support': True
+#     },
+#     'text': {
+#         'abs_coordinates': (0, None),
+#         'relative': ('text', 'number'),
+#         'type': 'TextBlock',
+#         'image': None,
+#         'max_lines': 3,
+#         'height': .4,
+#         'width': 1,
+#         'rand': True,
+#         'font': '../fonts/Open_Sans/OpenSans-Regular.ttf',
+#         'fill': 'ORANGE',
+#         'bkground': 'BLACK',
+#         'rgb_support': True
+#     }
+# }
+
+
+# # config = {
+# #     'resolution': [300, 200],
+# #     'max_priority': 1,
+# #     'refresh_rate': 2,
+# #     'update_function': bogus_plugin,
+# #     'layout': bogus_layout,
+# #     'screen_mode': 'RGB',
+# #     'plugin_timeout': 5,
+# #     'name': 'Bogus',
+# #     'foo': 'bar',
+# #     'spam': False
+# # }
+
+
+# #     Plugin.update_function = bogus_plugin
+
+# logger.root.setLevel('DEBUG')
+
+# l = Layout(resolution=(300, 200))
+# l.layout = bogus_layout
+# p = Layout(resolution=(800, 400))
+# p.layout = bogus_layout
+
+
+# # for i in range(5):
+# #     .resolution = (randint(300, 800), randint(300, 600))
+# #     logging.info(f'plugin resolution set to: {p.resolution}')
+# #     p.layout_obj = None
+# #     p.layout = bogus_layout
+# #     for s in bogus_layout:
+# #         colors = ['RED', 'ORANGE', 'YELLOW', 'GREEN', 'BLUE', 'BLACK', 'WHITE']
+# #         fill = choice(colors)
+# #         colors.remove(fill)
+# #         bkground = choice(colors)
+# #         p.layout_obj.update_block_props(block=s, props={'bkground': bkground, 'fill': fill}, force_recalc=True)
+
+# #     print('trying to update plugin')
+# #     p.force_update()
+# #     print('displaying image')
+# #     display(p.image)
+# # #         print('sleep for 1 second')
+# #     sleep(1)
+
+
+# print(f'l.layout: {l.resolution}')
+# for k, v in l.blocks.items():
+#     print(f'{k}\n   a: {v.area}\n   c: {v.abs_coordinates}')
+    
+# print(f'\n\np.layout: {p.resolution}')
+# for k, v in p.blocks.items():
+#     print(f'{k}\n   a: {v.area}\n   c: {v.abs_coordinates}')
+
+# p.resolution = (100, 40)
+
+# p._master_layout
+
+# print(f'l.layout: {l.resolution}')
+# for k, v in l.blocks.items():
+#     print(f'{k}\n   a: {v.area}\n   c: {v.abs_coordinates}')
+    
+# print(f'\n\np.layout: {p.resolution}')
+# for k, v in p.blocks.items():
+#     print(f'{k}\n   a: {v.area}\n   c: {v.abs_coordinates}')
 
 
